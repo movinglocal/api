3dc2dcc4a5e4688a0c6cf99f101f5cacce5d248b
g-div <g-div@anche.no>
2018-11-01 15:45:21 +0100

    feat(policy): add isOwnOrganisation policy

---
+++ plugins/users-permissions/config/policies/isOwnOrganisation.js
@@ -0,0 +1,10 @@
+module.exports = async (ctx, next) => {
+  if (ctx.state.user) {
+    if (ctx.state.user.organisation) {
+      const requestedOrganisation = ctx.request.url.replace('/organisations/', '');
+      const organisationId = ctx.state.user.organisation.id;
+      if (organisationId === requestedOrganisation) return next();
+    }
+  }
+  ctx.unauthorized('Invalid token.');
+};
