99bd885eca19165b94b85c43dca6967f3b5ff0fb
g-div <g-div@anche.no>
2018-11-27 10:16:39 +0100

    fix(policy): add console log statement for debugging

--- plugins/users-permissions/config/policies/isOwnOrganisation.js
+++ plugins/users-permissions/config/policies/isOwnOrganisation.js
@@ -1,6 +1,7 @@
 module.exports = async (ctx, next) => {
   if (ctx.state.user) {
     if (ctx.state.user.organisation) {
+      console.log(ctx.request.url, ctx.state.user)
       const requestedOrganisation = ctx.request.url.replace('/organisations/', '');
       const organisationId = ctx.state.user.organisation.id;
       if (organisationId === requestedOrganisation) return next();
